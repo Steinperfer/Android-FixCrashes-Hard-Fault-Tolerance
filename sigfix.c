4a5bde0f0e446160b2a15926c0b5aaab6d2be27f
Steinperfer <40601303+Steinperfer@users.noreply.github.com>
2026-05-10 06:14:13 +0200

    Add files via upload

--- sigfix.c
+++ sigfix.c
@@ -1,7 +1,13 @@
 #include <signal.h>
-void nocrash_handler(int sig) { return; }
+
+void nocrash_handler(int sig) {
+    return;
+}
+
 __attribute__((constructor))
 void install_handler() {
     signal(SIGBUS, nocrash_handler);
     signal(SIGILL, nocrash_handler);
+    signal(SIGSEGV, nocrash_handler);
+    signal(SIGTRAP, nocrash_handler);
 }
