9e2b73b0ad114118e798fbc86ad36733c764b23b
Steinperfer <40601303+Steinperfer@users.noreply.github.com>
2026-05-11 11:59:56 +0200

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,4 +1,2 @@
 #!/system/bin/sh
 export LD_PRELOAD=/system/lib64/libnocrash.so
-echo 0 > /sys/bus/platform/devices/17c10000.qcom,wdt/disable 2>/dev/null
-echo 1 > /sys/bus/platform/drivers/msm_watchdog/unbind 2>/dev/null
