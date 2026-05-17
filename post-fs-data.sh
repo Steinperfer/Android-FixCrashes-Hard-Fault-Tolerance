0d4ac0cb3454053a91f3ae0c0afa6d8573fb6a8f
Steinperfer <40601303+Steinperfer@users.noreply.github.com>
2026-05-17 21:52:50 +0200

    watchdog cant be disabled

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,21 +1,25 @@
 #!/system/bin/sh
 export LD_PRELOAD=/system/lib64/libnocrash.so
 
-# Ignore Kernel-Panics
+# Kernel-Panics verhindern
 echo 0 > /proc/sys/kernel/panic_on_oops 2>/dev/null
 echo 0 > /proc/sys/kernel/panic 2>/dev/null
 
-# OOM: Killapps before triggering a Kernel Panic
+# OOM: Apps killen statt Kernel-Panic
 echo 0 > /proc/sys/vm/panic_on_oom 2>/dev/null
 echo 1 > /proc/sys/vm/oom_kill_allocating_task 2>/dev/null
 echo 0 > /proc/sys/vm/oom_dump_tasks 2>/dev/null
 
-# RAM-Management
+# RAM-Management optimieren
 echo 1 > /proc/sys/vm/overcommit_memory 2>/dev/null
 echo 80 > /proc/sys/vm/overcommit_ratio 2>/dev/null
 echo 100 > /proc/sys/vm/vfs_cache_pressure 2>/dev/null
 
-# ZRAM to 6gb
+# Phantom-Process-Monitor deaktivieren
+setprop persist.sys.fflag.override.settings_enable_monitor_phantom_procs false
+settings put global settings_enable_monitor_phantom_procs false
+
+# ZRAM auf 6GB setzen
 if [ -f /sys/block/zram0/disksize ]; then
     swapoff /dev/block/zram0 2>/dev/null
     echo 1 > /sys/block/zram0/reset 2>/dev/null
