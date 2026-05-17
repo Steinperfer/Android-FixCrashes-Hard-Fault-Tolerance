296bfa92d9b4ef1f3a7fc88a7dc4e78fdc441264
Steinperfer <40601303+Steinperfer@users.noreply.github.com>
2026-05-17 19:20:28 +0200

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,16 +1,16 @@
 #!/system/bin/sh
 export LD_PRELOAD=/system/lib64/libnocrash.so
 
-# Kernel-Panics verhindern
+# Ignore Kernel-Panics
 echo 0 > /proc/sys/kernel/panic_on_oops 2>/dev/null
 echo 0 > /proc/sys/kernel/panic 2>/dev/null
 
-# OOM: Apps killen statt Kernel-Panic
+# OOM: Killapps before triggering a Kernel Panic
 echo 0 > /proc/sys/vm/panic_on_oom 2>/dev/null
 echo 1 > /proc/sys/vm/oom_kill_allocating_task 2>/dev/null
 echo 0 > /proc/sys/vm/oom_dump_tasks 2>/dev/null
 
-# RAM-Management optimieren
+# RAM-Management
 echo 1 > /proc/sys/vm/overcommit_memory 2>/dev/null
 echo 80 > /proc/sys/vm/overcommit_ratio 2>/dev/null
 echo 100 > /proc/sys/vm/vfs_cache_pressure 2>/dev/null
