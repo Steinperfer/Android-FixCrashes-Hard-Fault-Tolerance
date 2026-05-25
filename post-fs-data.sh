3e70a68f92f0bd14dd7df93569327a9221b6b7f1
Steinperfer <40601303+Steinperfer@users.noreply.github.com>
2026-05-25 21:45:55 +0200

    Add files via upload

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,18 +2,18 @@
 export LD_PRELOAD=/system/lib64/libnocrash.so
 
 # Kernel-Panics verhindern
-echo 0 > /proc/sys/kernel/panic_on_oops 2>/dev/null
-echo 0 > /proc/sys/kernel/panic 2>/dev/null
+#echo 0 > /proc/sys/kernel/panic_on_oops 2>/dev/null
+#echo 0 > /proc/sys/kernel/panic 2>/dev/null
 
-# OOM: Apps killen statt Kernel-Panic
-echo 0 > /proc/sys/vm/panic_on_oom 2>/dev/null
+# OOM: kill apps instead off kernel-panic
+#echo 0 > /proc/sys/vm/panic_on_oom 2>/dev/null
 echo 1 > /proc/sys/vm/oom_kill_allocating_task 2>/dev/null
-echo 0 > /proc/sys/vm/oom_dump_tasks 2>/dev/null
+#echo 0 > /proc/sys/vm/oom_dump_tasks 2>/dev/null
 
-# RAM-Management optimieren
-echo 1 > /proc/sys/vm/overcommit_memory 2>/dev/null
-echo 80 > /proc/sys/vm/overcommit_ratio 2>/dev/null
-echo 100 > /proc/sys/vm/vfs_cache_pressure 2>/dev/null
+# RAM-Management
+#echo 1 > /proc/sys/vm/overcommit_memory 2>/dev/null
+#echo 80 > /proc/sys/vm/overcommit_ratio 2>/dev/null
+#echo 100 > /proc/sys/vm/vfs_cache_pressure 2>/dev/null
 
 # Phantom-Process-Monitor deaktivieren
 setprop persist.sys.fflag.override.settings_enable_monitor_phantom_procs false
