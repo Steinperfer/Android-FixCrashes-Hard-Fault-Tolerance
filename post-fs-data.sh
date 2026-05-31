855c6d881052eabd538a005c8a90cc8f3aab4f9a
Steinperfer <40601303+Steinperfer@users.noreply.github.com>
2026-05-31 22:40:24 +0200

    Refactor post-fs-data.sh for memory management settings
    Removed commented-out kernel panic and OOM settings, and added new settings for aggressive low memory management and ANR timeout.

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,39 +1,21 @@
-#!/system/bin/sh
 export LD_PRELOAD=/system/lib64/libnocrash.so
 
-# Kernel-Panics verhindern
-#echo 0 > /proc/sys/kernel/panic_on_oops 2>/dev/null
-#echo 0 > /proc/sys/kernel/panic 2>/dev/null
-
-# OOM: kill apps instead off kernel-panic
-#echo 0 > /proc/sys/vm/panic_on_oom 2>/dev/null
-echo 1 > /proc/sys/vm/oom_kill_allocating_task 2>/dev/null
-#echo 0 > /proc/sys/vm/oom_dump_tasks 2>/dev/null
-
-# RAM-Management
-#echo 1 > /proc/sys/vm/overcommit_memory 2>/dev/null
-#echo 80 > /proc/sys/vm/overcommit_ratio 2>/dev/null
-#echo 100 > /proc/sys/vm/vfs_cache_pressure 2>/dev/null
-
-# Phantom-Process-Monitor deaktivieren
+# Disable Phantom Process Monitor
 setprop persist.sys.fflag.override.settings_enable_monitor_phantom_procs false
 settings put global settings_enable_monitor_phantom_procs false
 
-# ZRAM auf 2GB setzen
-#if [ -f /sys/block/zram0/disksize ]; then
-#    swapoff /dev/block/zram0 2>/dev/null
-#    echo 1 > /sys/block/zram0/reset 2>/dev/null
-#    echo 2147483648 > /sys/block/zram0/disksize 2>/dev/null
-#    mkswap /dev/block/zram0 2>/dev/null
-#    swapon /dev/block/zram0 2>/dev/null
-#fi
+# OOM: kill apps instead of kernel-panic
+echo 1 > /proc/sys/vm/oom_kill_allocating_task 2>/dev/null
 
-# Low Memory Killer aggressiver
+# Aggressive Low Memory Killer
 setprop ro.lmk.critical_upgrade true
 setprop ro.lmk.upgrade_pressure 60
 setprop ro.lmk.downgrade_pressure 80
 setprop ro.config.low_ram true
 
-# ANR-Timeout 5 Sekunden
+# ANR timeout 5 seconds
 setprop persist.sys.service_timeout 5000
 setprop persist.sys.broadcast_timeout 5000
+
+# Force audioserver to load signal handler
+setprop wrap.audioserver LD_PRELOAD=/system/lib64/libnocrash.so
