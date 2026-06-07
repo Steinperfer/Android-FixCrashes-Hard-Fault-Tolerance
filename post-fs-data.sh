4014687ffd22f9440280c2a7ee18ac9035603aa1
Steinperfer <40601303+Steinperfer@users.noreply.github.com>
2026-06-07 13:31:35 +0200

    Add files via upload

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -5,21 +5,9 @@ export LD_PRELOAD=/system/lib64/libnocrash.so
 setprop persist.sys.fflag.override.settings_enable_monitor_phantom_procs false
 settings put global settings_enable_monitor_phantom_procs false
 
-# OOM: kill apps instead of kernel-panic
-echo 1 > /proc/sys/vm/oom_kill_allocating_task 2>/dev/null
-
-# Aggressive Low Memory Killer
-setprop ro.lmk.critical_upgrade true
-setprop ro.lmk.upgrade_pressure 60
-setprop ro.lmk.downgrade_pressure 80
-setprop ro.config.low_ram true
-
 # ANR timeout 5 seconds
 setprop persist.sys.service_timeout 5000
 setprop persist.sys.broadcast_timeout 5000
 
 # Force audioserver to load signal handler
 setprop wrap.audioserver LD_PRELOAD=/system/lib64/libnocrash.so
-
-# Force ART JNI libraries to load (prevents ArtJni crash)
-setprop wrap.system_server LD_PRELOAD=/system/lib64/libnocrash.so:/system/lib64/libart.so
--- sigfix.c
+++ sigfix.c
@@ -1,13 +1,20 @@
 #include <signal.h>
+#include <stdlib.h>
 
 void nocrash_handler(int sig) {
+    // Komplett ignorieren, nichts tun, kein Log, kein Exit
     return;
 }
 
 __attribute__((constructor))
 void install_handler() {
-    signal(SIGBUS, nocrash_handler);
-    signal(SIGILL, nocrash_handler);
-    signal(SIGSEGV, nocrash_handler);
-    signal(SIGTRAP, nocrash_handler);
+    struct sigaction sa;
+    sa.sa_handler = nocrash_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_NODEFER | SA_RESTART;
+    sigaction(SIGBUS, &sa, NULL);
+    sigaction(SIGILL, &sa, NULL);
+    sigaction(SIGSEGV, &sa, NULL);
+    sigaction(SIGTRAP, &sa, NULL);
+    sigaction(SIGABRT, &sa, NULL);
 }
